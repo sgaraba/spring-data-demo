4718830746490196fde6fb456dcb669c3fcec033
Sergiu Garaba <sergiu.garaba@esempla.com>
2026-02-10 15:31:46 +0200

    config project

---
+++ src/main/java/md/utm2026/demo/SpringDataDemoApplication.java
@@ -0,0 +1,13 @@
+package md.utm2026.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataDemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringDataDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/md/utm2026/demo/SpringDataDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package md.utm2026.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
